d2980c02a2cb1e594c1b5ef9e223ecc66c61800e
Himanshu Bhardwaj <121593907+itz-himanshu128@users.noreply.github.com>
2025-06-21 11:29:26 +0000

    datatypes

---
+++ 01_basics/2_datatypes.js
@@ -0,0 +1,7 @@
+"use strict"; // it means that the below code will be of newer version of code 
+// alert(3+3)  // we are using node js not browse so this will not work 
+let name = "himanshu"    // string 
+let age = 20            // int
+let islogged =true     //bool
+console.log( typeof(null))          // is an object
+console.log( typeof(undefined))     //it's type is undefined 
