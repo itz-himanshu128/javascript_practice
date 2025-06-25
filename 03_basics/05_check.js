e51e94a5fdcd3e1eb6bdeec1775a69e4e6bcb9d4
Himanshu Bhardwaj <121593907+itz-himanshu128@users.noreply.github.com>
2025-06-25 13:18:33 +0000

    learn foe loop and how to take obj from array using loop(forEach loop)

---
+++ 03_basics/05_check.js
@@ -0,0 +1,12 @@
+// to check array is empty or not 
+let array = []
+
+if (array.length===0) {
+    console.log("empty array");
+    
+} else {
+    console.log(`${array}`);
+    
+}
+// console.log("working");
+// console.log(`${array}`);
---
+++ 03_basics/06_foreach.js
@@ -0,0 +1,19 @@
+let myCoding=[
+    {
+        languageName: "java",
+        languageFileName:"java"
+    },
+    {
+        languageName: "javascript",
+        languageFileName:"js"
+    },
+    {
+        languageName: "python",
+        languageFileName:"py"
+    }
+]
+myCoding.forEach((item)=>{
+    console.log(item.languageName);
+    console.log(item.languageFileName);
+    
+})
